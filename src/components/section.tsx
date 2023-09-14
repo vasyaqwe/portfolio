4049f409ebdba70d02886bae408f936a7e2c5969
vasyaqwe <vasyaqwe>
2023-09-14 09:16:45 +0300

    docs: update readme

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -28,7 +28,6 @@ export function Section({
 
     useEffect(() => {
         router.push(`#${activeSection.toLocaleLowerCase()}`)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     useEffect(() => {
