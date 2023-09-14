149d52167f40c0f8601cdeee4856a5cc25dd4607
vasyaqwe <vasyaqwe>
2023-09-14 10:20:49 +0300

    feat: remove swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/ui/icons"
 import avatar from "@public/images/avatar.jpg"
@@ -9,12 +8,9 @@ import { ContactForm } from "@/components/forms/contact-form"
 import { projects, skills } from "@/config"
 import { Project } from "@/components/project"
 import { Section } from "@/components/section"
+import { Slider } from "@/components/slider"
 import { Pill } from "@/components/ui/pill"
 import { DividerArrow } from "@/components/ui/divider-arrow"
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css"
-import "swiper/css/free-mode"
-import { FreeMode } from "swiper/modules"
 
 export default function Home() {
     return (
@@ -141,22 +137,14 @@ export default function Home() {
                 <h2 className="mb-3 text-center text-size-800 md:mb-8">
                     My projects
                 </h2>
-                <Swiper
-                    slidesPerView={"auto"}
-                    spaceBetween={16}
-                    freeMode={true}
-                    modules={[FreeMode]}
-                    className="mySwiper"
-                >
+                <Slider>
                     {projects.map((project) => (
-                        <SwiperSlide
-                            className="!w-[320px] md:!w-[740px]"
+                        <Project
                             key={project.name}
-                        >
-                            <Project project={project} />
-                        </SwiperSlide>
+                            project={project}
+                        />
                     ))}
-                </Swiper>
+                </Slider>
             </Section>
             <Section
                 section="Skills"
