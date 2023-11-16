0ad9f28417c5360ba2b2e853d63f2766423cbc02
vasyaqwe <vasyaqwe>
2023-11-16 12:57:52 +0200

    chore: update packages

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,10 +3,12 @@ import { extendTailwindMerge } from "tailwind-merge"
 import { extendedTheme } from "./extended-theme"
 
 const customTwMerge = extendTailwindMerge({
-    classGroups: {
-        "font-size": Object.keys(extendedTheme.fontSize).map(
-            (key) => `text-${key}`
-        ),
+    extend: {
+        classGroups: {
+            "font-size": Object.keys(extendedTheme.fontSize).map(
+                (key) => `text-${key}`
+            ),
+        },
     },
 })
 
